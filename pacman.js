8350fafe56b1c218bd2865b7a3342cdd81ad517a
Piotr Gularski <piotr.gularski@gmail.com>
2016-02-25 23:23:37 +0000

    Pacman is moved to a separate file. Needs cleaning

--- pacman.js
+++ pacman.js
@@ -4,16 +4,149 @@
 Pacman = function (game, x, y) {
     var self = this;
     Phaser.Sprite.call(self, game, x, y, 'pacman');
-    self.game = game;
+    // FIXME: Why it doesn't work?
+    //self.game = game;
+    self.game = game.state.states.Game;
+    // FIXME: How to get the actual map object from the game object?
+    //self.map = self.game.map;
+    self.map = game.state.states.Game.map;
+
+    // FIXME: How to get the actual layer object from the game object?
+    //self.layer = self.game.layer;
+    self.layer = game.state.states.Game.layer;
 
     self.anchor.set(0.5);
     self.animations.add("eat", [0, 1, 2, 1], 10, true);
     self.play("eat");
 
+    self.speed = 150;
+
     self.game.physics.arcade.enable(self);
     self.body.setSize(32, 32, 0, 0);
 
+    // FIXME: Isn't that too implicit?
     self.game.add.existing(self);
+
+    self.directions = [ null, null, null, null, null ];
+
+    self.turnPoint = new Phaser.Point();
+    self.marker = new Phaser.Point();
+
+    self.current = Phaser.NONE;
+    self.turning = Phaser.NONE;
+
+    // TODO: This should not be hardcoded.
+    self.safetile = 1;
+
+    self.opposites = [ Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP ];
 };
+
 Pacman.prototype = Object.create(Phaser.Sprite.prototype);
 Pacman.prototype.constructor = Pacman;
+
+Pacman.prototype.checkDirection = function (turnTo) {
+    var self = this;
+    if (self.turning === turnTo || self.directions[turnTo] === null || self.directions[turnTo].index !== self.safetile)
+    {
+        return;
+    }
+
+    //  Check if they want to turn around and can
+    if (self.current === self.opposites[turnTo])
+    {
+        self.move(turnTo);
+    }
+    else
+    {
+        self.turning = turnTo;
+
+        self.turnPoint.x = (self.marker.x * self.gridsize) + (self.gridsize / 2);
+        self.turnPoint.y = (self.marker.y * self.gridsize) + (self.gridsize / 2);
+    }
+};
+
+
+Pacman.prototype._update = function () {
+    var self = this;
+
+    // It's in the grid coordinates, not in pixels
+    // TODO: Use some built-in methods.
+    self.marker = self.game.getObjectGridPoint(self);
+
+    self.directions[Phaser.LEFT] = self.map.getTileLeft(self.layer.index, self.marker.x, self.marker.y);
+    self.directions[Phaser.RIGHT] = self.map.getTileRight(self.layer.index, self.marker.x, self.marker.y);
+    self.directions[Phaser.UP] = self.map.getTileAbove(self.layer.index, self.marker.x, self.marker.y);
+    self.directions[Phaser.DOWN] = self.map.getTileBelow(self.layer.index, self.marker.x, self.marker.y);
+};
+
+Pacman.prototype.isInTurnPoint = function (object) {
+    var self = this;
+    var objectGridPoint = self.getObjectGridPoint(object);
+    var currentX = Math.floor(object.x);
+    var currentY = Math.floor(object.y);
+    if (self.math.fuzzyEqual(currentX, self.turnPoint.x, self.threshold) &&
+        self.math.fuzzyEqual(currentY, self.turnPoint.y, self.threshold)){
+        return true;
+    }
+    return false;
+}
+
+
+Pacman.prototype.turn = function () {
+    var self = this;
+
+    if (!self.game.isInTurnPoint(self)) {
+        return false;
+    }
+
+    //  Grid align before turning
+    self.game.alignToTile(self);
+
+    self.move(self.turning);
+
+    self.turning = Phaser.NONE;
+
+    return true;
+
+}
+
+
+Pacman.prototype.move = function (direction) {
+    var self = this;
+
+    var speed = self.speed;
+
+    if (direction === Phaser.LEFT || direction === Phaser.UP)
+    {
+        speed = -speed;
+    }
+
+    if (direction === Phaser.LEFT || direction === Phaser.RIGHT)
+    {
+        self.body.velocity.x = speed;
+    }
+    else
+    {
+        self.body.velocity.y = speed;
+    }
+
+    //  Reset the scale and angle (Pacman is facing to the right in the sprite sheet)
+    self.scale.x = 1;
+    self.angle = 0;
+
+    if (direction === Phaser.LEFT)
+    {
+        self.scale.x = -1;
+    }
+    else if (direction === Phaser.UP)
+    {
+        self.angle = 270;
+    }
+    else if (direction === Phaser.DOWN)
+    {
+        self.angle = 90;
+    }
+
+    self.current = direction;
+}
+
--- pacmangame.js
+++ pacmangame.js
@@ -80,10 +80,6 @@ var PacmanGame = function(game) {
     self.speed = 150;
     self.threshold = 5;
 
-
-    self.marker = new Phaser.Point();
-    self.turnPoint = new Phaser.Point();
-
     // Ghost properties
     // TODO: Move to external object.
     self.ghostSpeed = 150;
@@ -95,11 +91,7 @@ var PacmanGame = function(game) {
     self.ghostTurns = [];
     self.ghostLastTurn = null;
 
-    self.directions = [ null, null, null, null, null ];
-    self.opposites = [ Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP ];
 
-    self.current = Phaser.NONE;
-    self.turning = Phaser.NONE;
 }
 
 PacmanGame.prototype.init = function () {
@@ -143,7 +135,7 @@ PacmanGame.prototype.create = function () {
     self.debugKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
     self.debugKey.isPressed = false;
 
-    self.move(Phaser.RIGHT);
+    self.pacman.move(Phaser.RIGHT);
 
     // Trigger gameCreated to start tests.
     // TODO: There's definitely some better way of doing that.
@@ -157,21 +149,15 @@ PacmanGame.prototype.update = function () {
     self.physics.arcade.collide(self.ghost, self.layer);
 
     // It's in the grid coordinates, not in pixels
-    self.marker = self.getObjectGridPoint(self.pacman);
-
-    // It's in the grid coordinates, not in pixels
     self.ghostMarker = self.getObjectGridPoint(self.ghost);
 
-    self.directions[Phaser.LEFT] = self.map.getTileLeft(self.layer.index, self.marker.x, self.marker.y);
-    self.directions[Phaser.RIGHT] = self.map.getTileRight(self.layer.index, self.marker.x, self.marker.y);
-    self.directions[Phaser.UP] = self.map.getTileAbove(self.layer.index, self.marker.x, self.marker.y);
-    self.directions[Phaser.DOWN] = self.map.getTileBelow(self.layer.index, self.marker.x, self.marker.y);
+    self.pacman._update();
 
     self.checkKeys();
 
-    if (self.turning !== Phaser.NONE)
+    if (self.pacman.turning !== Phaser.NONE)
     {
-        self.turn();
+        self.pacman.turn();
     }
 
     // TODO: update path only when pacman enters a new tile.
@@ -324,45 +310,6 @@ PacmanGame.prototype.getObjectGridY = function (obj) {
 };
 
 
-PacmanGame.prototype.move = function (direction) {
-    var self = this;
-
-    var speed = self.speed;
-
-    if (direction === Phaser.LEFT || direction === Phaser.UP)
-    {
-        speed = -speed;
-    }
-
-    if (direction === Phaser.LEFT || direction === Phaser.RIGHT)
-    {
-        self.pacman.body.velocity.x = speed;
-    }
-    else
-    {
-        self.pacman.body.velocity.y = speed;
-    }
-
-    //  Reset the scale and angle (Pacman is facing to the right in the sprite sheet)
-    self.pacman.scale.x = 1;
-    self.pacman.angle = 0;
-
-    if (direction === Phaser.LEFT)
-    {
-        self.pacman.scale.x = -1;
-    }
-    else if (direction === Phaser.UP)
-    {
-        self.pacman.angle = 270;
-    }
-    else if (direction === Phaser.DOWN)
-    {
-        self.pacman.angle = 90;
-    }
-
-    self.current = direction;
-}
-
 PacmanGame.prototype.checkGhostDirection = function () {
     var x = self.ghostMarker.x;
     var y = self.ghostMarker.y;
@@ -438,39 +385,27 @@ PacmanGame.prototype.isInTurnPoint  = function (object) {
 }
 
 
-PacmanGame.prototype.isInTurnPointPacman  = function (object) {
-    var self = this;
-    var objectGridPoint = self.getObjectGridPoint(object);
-    var currentX = Math.floor(object.x);
-    var currentY = Math.floor(object.y);
-    if (self.math.fuzzyEqual(currentX, self.turnPoint.x, self.threshold) &&
-        self.math.fuzzyEqual(currentY, self.turnPoint.y, self.threshold)){
-        return true;
-    }
-    return false;
-}
-
 
 PacmanGame.prototype.checkKeys = function () {
     var self = this;
 
-    if (self.cursors.left.isDown && self.current !== Phaser.LEFT) {
-        self.checkDirection(Phaser.LEFT);
+    if (self.cursors.left.isDown && self.pacman.current !== Phaser.LEFT) {
+        self.pacman.checkDirection(Phaser.LEFT);
     }
-    else if (self.cursors.right.isDown && self.current !== Phaser.RIGHT) {
-        self.checkDirection(Phaser.RIGHT);
-    }
-
-    else if (self.cursors.up.isDown && self.current !== Phaser.UP) {
-        self.checkDirection(Phaser.UP);
+    else if (self.cursors.right.isDown && self.pacman.current !== Phaser.RIGHT) {
+        self.pacman.checkDirection(Phaser.RIGHT);
     }
 
-    else if (self.cursors.down.isDown && self.current !== Phaser.DOWN) {
-        self.checkDirection(Phaser.DOWN);
+    else if (self.cursors.up.isDown && self.pacman.current !== Phaser.UP) {
+        self.pacman.checkDirection(Phaser.UP);
+    }
+
+    else if (self.cursors.down.isDown && self.pacman.current !== Phaser.DOWN) {
+        self.pacman.checkDirection(Phaser.DOWN);
     }
     else
     {
-        self.turning = Phaser.NONE;
+        self.pacman.turning = Phaser.NONE;
     }
 
     if (self.debugKey.isDown && !self.debugKey.isPressed) {
@@ -482,69 +417,6 @@ PacmanGame.prototype.checkKeys = function () {
         self.debugKey.isPressed = false;
     }
 }
-
-PacmanGame.prototype.checkDirection = function (turnTo) {
-    var self = this;
-    if (self.turning === turnTo || self.directions[turnTo] === null || self.directions[turnTo].index !== self.safetile)
-    {
-        return;
-    }
-
-    //  Check if they want to turn around and can
-    if (self.current === self.opposites[turnTo])
-    {
-        self.move(turnTo);
-    }
-    else
-    {
-        self.turning = turnTo;
-
-        self.turnPoint.x = (self.marker.x * self.gridsize) + (self.gridsize / 2);
-        self.turnPoint.y = (self.marker.y * self.gridsize) + (self.gridsize / 2);
-    }
-}
-
-PacmanGame.prototype.turn = function () {
-    var self = this;
-
-    if (!self.isInTurnPointPacman(self.pacman)) {
-        return false;
-    }
-
-    //  Grid align before turning
-    self.alignToTile(self.pacman);
-
-    self.move(self.turning);
-
-    self.turning = Phaser.NONE;
-
-    return true;
-
-}
-
-PacmanGame.prototype.turn_orig = function () {
-    var self = this;
-
-    var cx = Math.floor(self.pacman.x);
-    var cy = Math.floor(self.pacman.y);
-
-    //  This needs a threshold, because at high speeds you can't turn because the coordinates skip past
-    if (!self.math.fuzzyEqual(cx, self.turnPoint.x, self.threshold) || !self.math.fuzzyEqual(cy, self.turnPoint.y, self.threshold))
-    {
-        return false;
-    }
-
-    //  Grid align before turning
-    self.alignToTile(self.pacman);
-
-    self.move(self.turning);
-
-    self.turning = Phaser.NONE;
-
-    return true;
-
-}
-
 
 PacmanGame.prototype.moveGhost2 = function () {
     var self = this;
