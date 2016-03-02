5618905084e8d640ed908038b1af6491d428d31a
Piotr Gularski <piotr.gularski@gmail.com>
2016-03-02 00:10:23 +0000

    Ghosts can use chasing strategies now

--- ghost.js
+++ ghost.js
@@ -7,7 +7,130 @@ var randomize = function(number, range) {
 };
 
 
-Ghost = function (pacmanGameState, game, x, y) {
+var getRandomizedTargetTile = function (ghost, target, factor) {
+    var point = new Phaser.Point(
+            randomize(target.x, ghost.width * factor),
+            randomize(target.y, ghost.width * factor)
+    );
+    var virtualTarget = ghost.game.getPointTile(point);
+    if (virtualTarget && ghost.game.isSafeTile(virtualTarget)) {
+        return virtualTarget;
+    }
+    return ghost.game.getObjectTile(target);
+};
+
+
+StraightToThePointChasing = function (ghost) {
+    var self = this;
+    self.ghost = ghost;
+};
+
+StraightToThePointChasing.prototype.chase = function (target) {
+    var self = this;
+    var ghost = self.ghost;
+
+    if (ghost.checkpoints.length === 0 || ghost.game.isJunction(ghost.game.getObjectTile(ghost)) ) {
+        ghost.updateCheckPoints(ghost.game.getObjectTile(target));
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.currentCheckpointTile = ghost.map.getTile(
+            ghost.currentCheckpoint.x, ghost.currentCheckpoint.y)
+        ghost.setDirection();
+    }
+    var x = ghost.x - (ghost.body.width * ghost.anchor.x);
+    var y = ghost.y - (ghost.body.height * ghost.anchor.y);
+
+    //var currentCheckpointPoint = new Phaser.Point(
+            //currentCheckpointTile.worldX, currentCheckpointTile.worldY);
+    //game.debug.geom(currentCheckpointPoint, '#ffff00');
+
+    var distance = ghost.game.math.distance(
+            x, y,
+            ghost.currentCheckpointTile.worldX, ghost.currentCheckpointTile.worldY);
+
+    if (distance <= ghost.MAX_DISTANCE) {
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.setDirection();
+    }
+};
+
+
+SlightlyRandomizedChasing = function (ghost) {
+    var self = this;
+    self.ghost = ghost;
+};
+
+
+SlightlyRandomizedChasing.prototype.chase = function (target) {
+    var self = this;
+    var ghost = self.ghost;
+
+    var virtualTarget = getRandomizedTargetTile(ghost, target, 10);
+
+    if (ghost.checkpoints.length === 0 || ghost.game.isJunction(ghost.game.getObjectTile(ghost))
+        || (ghost.body.deltaX() === 0 && ghost.body.deltaY() ===0)) {
+        ghost.updateCheckPoints(virtualTarget);
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.currentCheckpointTile = ghost.map.getTile(
+            ghost.currentCheckpoint.x, ghost.currentCheckpoint.y)
+        ghost.setDirection();
+    }
+    var x = ghost.x - (ghost.body.width * ghost.anchor.x);
+    var y = ghost.y - (ghost.body.height * ghost.anchor.y);
+
+    //var currentCheckpointPoint = new Phaser.Point(
+            //currentCheckpointTile.worldX, currentCheckpointTile.worldY);
+    //game.debug.geom(currentCheckpointPoint, '#ffff00');
+
+    var distance = ghost.game.math.distance(
+            x, y,
+            ghost.currentCheckpointTile.worldX, ghost.currentCheckpointTile.worldY);
+
+    if (distance <= ghost.MAX_DISTANCE) {
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.setDirection();
+    }
+};
+
+
+RandomizedChasing = function (ghost) {
+    var self = this;
+    self.ghost = ghost;
+};
+
+
+RandomizedChasing.prototype.chase = function (target) {
+    var self = this;
+    var ghost = self.ghost;
+
+    var virtualTarget = getRandomizedTargetTile(ghost, target, 20);
+
+    if (ghost.checkpoints.length === 0 || ghost.game.isJunction(ghost.game.getObjectTile(ghost))
+        || (ghost.body.deltaX() === 0 && ghost.body.deltaY() ===0)) {
+        ghost.updateCheckPoints(virtualTarget);
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.currentCheckpointTile = ghost.map.getTile(
+            ghost.currentCheckpoint.x, ghost.currentCheckpoint.y)
+        ghost.setDirection();
+    }
+    var x = ghost.x - (ghost.body.width * ghost.anchor.x);
+    var y = ghost.y - (ghost.body.height * ghost.anchor.y);
+
+    //var currentCheckpointPoint = new Phaser.Point(
+            //currentCheckpointTile.worldX, currentCheckpointTile.worldY);
+    //game.debug.geom(currentCheckpointPoint, '#ffff00');
+
+    var distance = ghost.game.math.distance(
+            x, y,
+            ghost.currentCheckpointTile.worldX, ghost.currentCheckpointTile.worldY);
+
+    if (distance <= ghost.MAX_DISTANCE) {
+        ghost.currentCheckpoint = ghost.checkpoints.pop();
+        ghost.setDirection();
+    }
+};
+
+
+Ghost = function (pacmanGameState, game, x, y, chasingStrategy) {
     var self = this;
     Phaser.Sprite.call(self, game, x, y, 'ghost');
 
@@ -40,86 +163,16 @@ Ghost = function (pacmanGameState, game, x, y) {
     self.threshold = 5;
     self.MAX_DISTANCE = 3;
 
-
-}
+    self.chasingStrategy = new chasingStrategy(self);
+};
 Ghost.prototype = Object.create(Phaser.Sprite.prototype);
 Ghost.prototype.constructor = Ghost;
 
+
 Ghost.prototype.update = function () {
     var self = this;
-
-    //self.move();
-    self.chase2(self.game.pacman);
-}
-
-Ghost.prototype.chase = function (target) {
-    var self = this;
-
-    if (self.checkpoints.length === 0 || self.game.isJunction(self.game.getObjectTile(self)) ) {
-        self.updateCheckPoints(self.game.getObjectTile(target));
-        self.currentCheckpoint = self.checkpoints.pop();
-        self.currentCheckpointTile = self.map.getTile(
-            self.currentCheckpoint.x, self.currentCheckpoint.y)
-        self.setDirection();
-    }
-    var x = self.x - (self.body.width * self.anchor.x);
-    var y = self.y - (self.body.height * self.anchor.y);
-
-    //var currentCheckpointPoint = new Phaser.Point(
-            //currentCheckpointTile.worldX, currentCheckpointTile.worldY);
-    //game.debug.geom(currentCheckpointPoint, '#ffff00');
-
-    var distance = self.game.math.distance(
-            x, y,
-            self.currentCheckpointTile.worldX, self.currentCheckpointTile.worldY);
-
-    if (distance <= self.MAX_DISTANCE) {
-        self.currentCheckpoint = self.checkpoints.pop();
-        self.setDirection();
-    }
-}
-
-Ghost.prototype.getRandomizedTargetTile = function (target) {
-    var self = this;
-    var point = new Phaser.Point(
-            randomize(target.x, self.width * 10),
-            randomize(target.y, self.width * 10)
-    );
-    var virtualTarget = self.game.getPointTile(point);
-    if (virtualTarget && self.game.isSafeTile(virtualTarget)) {
-        return virtualTarget;
-    }
-    return self.game.getObjectTile(target);
-}
-
-Ghost.prototype.chase2 = function (target) {
-    var self = this;
-    var virtualTarget = self.getRandomizedTargetTile(target);
-
-    if (self.checkpoints.length === 0 || self.game.isJunction(self.game.getObjectTile(self))
-        || (self.body.deltaX() === 0 && self.body.deltaY() ===0)) {
-        self.updateCheckPoints(virtualTarget);
-        self.currentCheckpoint = self.checkpoints.pop();
-        self.currentCheckpointTile = self.map.getTile(
-            self.currentCheckpoint.x, self.currentCheckpoint.y)
-        self.setDirection();
-    }
-    var x = self.x - (self.body.width * self.anchor.x);
-    var y = self.y - (self.body.height * self.anchor.y);
-
-    //var currentCheckpointPoint = new Phaser.Point(
-            //currentCheckpointTile.worldX, currentCheckpointTile.worldY);
-    //game.debug.geom(currentCheckpointPoint, '#ffff00');
-
-    var distance = self.game.math.distance(
-            x, y,
-            self.currentCheckpointTile.worldX, self.currentCheckpointTile.worldY);
-
-    if (distance <= self.MAX_DISTANCE) {
-        self.currentCheckpoint = self.checkpoints.pop();
-        self.setDirection();
-    }
-}
+    self.chasingStrategy.chase(self.game.pacman);
+};
 
 
 Ghost.prototype.setDirection = function () {
@@ -142,7 +195,8 @@ Ghost.prototype.setDirection = function () {
     else if (selfTileXY.y > self.currentCheckpoint.y) {
         self.body.velocity.y = -speed;
     }
-}
+};
+
 
 Ghost.prototype.updateCheckPoints = function (targetTile) {
     var self = this;
@@ -158,4 +212,4 @@ Ghost.prototype.updateCheckPoints = function (targetTile) {
     //checkpoints.unshift(self.game.getPointTileXY(target.position));
     checkpoints.unshift(new Phaser.Point(targetTile.x, targetTile.y));
     self.checkpoints = checkpoints;
-}
+};
