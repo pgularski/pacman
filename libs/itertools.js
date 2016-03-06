44874124e95ad4734752085e3086ad1ffdce1ab1
Piotr Gularski <piotr.gularski@gmail.com>
2016-03-06 22:50:55 +0000

    itertools added

---
+++ libs/itertools.js
@@ -0,0 +1,37 @@
+var itertools = itertools || {};
+
+
+itertools.makeIterator = function(array){
+    var nextIndex = 0;
+
+    return {
+        next: function(){
+            return nextIndex < array.length ?
+                {value: array[nextIndex++], done: false} :
+                {done: true};
+        }
+    }
+}
+
+itertools.cycle = function (array) {
+    var Cycle = function (array) {
+        var self = this;
+        self.index = 0;
+        self.array = array;
+    }
+
+    Cycle.prototype.next = function () {
+        var self = this;
+        while (true) {
+            if (self.index >= self.array.length) {
+                self.index = 0;
+            }
+            return self.array[self.index++];
+        }
+    }
+    return new Cycle(array);
+}
+
+
+//a = [1,2,3];
+//c = itertools.cycle(a);
