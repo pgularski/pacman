1352ae1d6cfa50138079180f6413e9489ed47030
Piotr Gularski <piotr.gularski@gmail.com>
2016-03-31 00:03:21 +0100

    itertools is more of an actual js lib

--- libs/itertools.js
+++ libs/itertools.js
@@ -1,27 +1,39 @@
-var itertools = itertools || {};
+(function() {
+    var root = this;
+    var itertools = {};
 
-
-itertools.makeIterator = function(array){
-    var nextIndex = 0;
-
-    return {
-        next: function(){
-            return nextIndex < array.length ?
-                {value: array[nextIndex++], done: false} :
-                {done: true};
+    // Export the itertools to Node.js, as in Underscore.js
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = itertools;
         }
+        exports.itertools = itertools;
+    } else {
+        root.itertools = itertools;
     }
-}
 
+    // From MDN
+    itertools.makeIterator = function(array) {
+        var nextIndex = 0;
 
-itertools.cycle = function(array) {
-    var index = 0;
-    return {
-        next: function(){
-            if (index === array.length) {
-                index = 0;
+        return {
+            next: function () {
+                return nextIndex < array.length ?
+                    {value: array[nextIndex++], done: false} :
+                    {done: true};
             }
-            return array[index++]
         }
-    }
-}
+    };
+
+    itertools.cycle = function (array) {
+        var index = 0;
+        return {
+            next: function () {
+                if (index === array.length) {
+                    index = 0;
+                }
+                return array[index++];
+            }
+        }
+    };
+}.call(this));
